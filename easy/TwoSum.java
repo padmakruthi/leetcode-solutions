e93b70b127b30c10dc6579155e59197debf1309c
Kruthi <aes16kruthi16srinivas@gmail.com>
2025-11-22 19:27:35 +0530

    Update TwoSum.java

--- easy/TwoSum.java
+++ easy/TwoSum.java
@@ -10,19 +10,6 @@ class TwoSum{
         }
         return new int[] {};
     }
-
-    public static void main(String args[]) {
-        TwoSum obj = new TwoSum();
-
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-
-        int[] result = obj.twoSum(nums, target);
-
-        if(result.length == 0) {
-            System.out.println("No pair found");
-        } else {
-            System.out.println("[" + result[0] + ", " + result[1]+"]");
-        }
-    }
 }
+
+    
